0bea4a260695e244885e3b4f0f14e4dbc99c9f76
Teyeb <hussenab@gmail.com>
2021-12-26 03:54:54 +0200

    client_id

--- src/components/GoogleLogin/index.tsx
+++ src/components/GoogleLogin/index.tsx
@@ -12,7 +12,7 @@ require('dotenv').config()
 
 function Login() {
   //const service = new Service()
-  const REACT_APP_client_id : string | any = process.env.REACT_APP_client_id
+  const client_id : string | any = process.env.client_id
   
   const dispatch = useDispatch()
   const onSuccess = async (response: any) => {
@@ -41,7 +41,7 @@ function Login() {
   return (
     <div className='login'>
       <GoogleLogin
-        clientId={REACT_APP_client_id}
+        clientId={client_id}
         buttonText="Login using your google acount"
         onSuccess={onSuccess}
         onFailure={onFailure}
